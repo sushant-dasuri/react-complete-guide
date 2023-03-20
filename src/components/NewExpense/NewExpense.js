5464b131e4ba31c9000fb0e5fcaf881db20623d8
sushant-dasuri <sushant.dasuri@more.global>
2023-03-20 17:12:35 +0530

    Assignment completed - Conditional Content

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,20 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
 const NewExpense = (props) => {
+  const [editForm, setEditForm] = useState(false);
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString()
     };
     props.onAddExpense(expenseData);
+    setEditForm(false);
   };
+
+  const noSubmitHandler = () => {
+    setEditForm(false);
+  }
+
+  const toggleFormHandler = () => {
+    setEditForm(true);
+  }
+
+  if(!editForm) {
+    return (
+      <div className='new-expense'>
+      <button onClick={toggleFormHandler}>Add Expenses</button>
+    </div>
+    )
+  }
 
   return (
     <div className='new-expense'>
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel={noSubmitHandler} />
     </div>
   );
 };
