7932f4037185a282a6761c93b0978a10328ec9f1
sushant-dasuri <sushant.dasuri@more.global>
2023-03-31 21:01:10 +0530

    Adding Errormodal component

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import Card from '../UI/Card';
 import Button from '../UI/Button';
 import classes from './AddUser.module.css';
+import ErrorModal from '../UI/ErrorModal';
 
 const AddUser = (props) => {
 
@@ -33,6 +34,8 @@ const AddUser = (props) => {
     }
 
     return(
+       <div>
+        <ErrorModal title="An error occured" message="Something went wrong" />
         <Card className={classes.input}>
             <form onSubmit={addUserHandler}>
             <label htmlFor="username">Username</label>
@@ -42,6 +45,7 @@ const AddUser = (props) => {
             <Button type="submit">Add User</Button>
         </form>
         </Card>
+       </div>
     );
 };
 
