1a654ab0bb3202c5b48d0a1b5bcb7e1530f6ec33
sushant-dasuri <sushant.dasuri@more.global>
2023-06-20 19:16:53 +0530

    component lifecycle starting method

--- src/components/Users.js
+++ src/components/Users.js
@@ -3,12 +3,6 @@ import User from './User';
 
 import classes from './Users.module.css';
 
-const DUMMY_USERS = [
-  { id: 'u1', name: 'Max' },
-  { id: 'u2', name: 'Manuel' },
-  { id: 'u3', name: 'Julie' },
-];
-
 // const Users = () => {
 //   const [showUsers, setShowUsers] = useState(true);
 
@@ -55,7 +49,7 @@ class Users extends Component {
 
   const usersList = (
     <ul>
-      {DUMMY_USERS.map((user) => (
+      {this.props.users.map((user) => (
         <User key={user.id} name={user.name} />
       ))}
     </ul>
