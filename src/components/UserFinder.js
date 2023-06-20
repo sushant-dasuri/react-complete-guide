9db12451f526cd05e2c5530e489651bb22703213
sushant-dasuri <sushant.dasuri@more.global>
2023-06-20 23:19:51 +0530

    Class based components and context

--- src/components/UserFinder.js
+++ src/components/UserFinder.js
@@ -1,6 +1,7 @@
 import { Fragment, useState, useEffect, Component } from 'react';
 
 import Users from './Users';
+import UsersContext from '../store/users-context';
 import classes from './UserFinder.module.css';
 
 const DUMMY_USERS = [
@@ -10,22 +11,24 @@ const DUMMY_USERS = [
 ];
 
 class UserFinder extends Component {
+    static contextType = UsersContext;
+
     constructor() {
         super();
         this.state = {
-            filteredUsers: DUMMY_USERS,
+            filteredUsers: this.context.users,
             searchTerm: ''
         }
     }
 
     componentDidMount() {
-        this.setState({filteredUsers: DUMMY_USERS});
+        this.setState({filteredUsers: this.context.users});
     }
 
     componentDidUpdate(prevProps, prevState) {
       if(prevState.searchTerm !== this.state.searchTerm) {
         this.setState({
-            filteredUsers: DUMMY_USERS.filter((user) => user.name.includes(this.state.searchTerm))
+            filteredUsers: this.context.users.filter((user) => user.name.includes(this.state.searchTerm))
         })
       }
     }
@@ -37,6 +40,9 @@ class UserFinder extends Component {
     render() {
         return (
             <Fragment>
+                <UsersContext.Consumer>
+
+                </UsersContext.Consumer>
               <div className={classes.finder}>
                 <input type='search' onChange={this.searchChangeHandler.bind(this)} />
               </div>
