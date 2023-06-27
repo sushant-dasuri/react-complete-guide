0705be3259d6fecc54662c671242e79fed577f27
sushant-dasuri <sushant.dasuri@more.global>
2023-06-27 16:04:01 +0530

    Handling Errors

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -7,10 +7,16 @@ const AvailableMeals = () => {
 
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
 
   useEffect(() => {
     const fetchMeals =async() => {
       const response = await fetch('https://react-http-ae176-default-rtdb.firebaseio.com/meals.json');
+
+      if(!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+
       const responseData = await response.json();
 
       const loadedMeals = [];
@@ -27,7 +33,10 @@ const AvailableMeals = () => {
       setIsLoading(false);
     };
 
-    fetchMeals();
+    fetchMeals().catch(error => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
   }, []);
 
   if(isLoading) {
@@ -35,6 +44,14 @@ const AvailableMeals = () => {
       <section className={classes.MealsLoading}>
         <p>Loading...</p>
       </section>
+    )
+  }
+
+  if(httpError) {
+    return (
+      <section className={classes.MealsError}>
+      <p>{httpError}</p>
+    </section>
     )
   }
 
