bd507370afb88291a2498c2e90b44169da741263
sushant-dasuri <sushant.dasuri@more.global>
2023-06-27 15:28:09 +0530

    Handling the loading state

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -6,6 +6,7 @@ import classes from './AvailableMeals.module.css';
 const AvailableMeals = () => {
 
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchMeals =async() => {
@@ -23,10 +24,19 @@ const AvailableMeals = () => {
         })
       }
       setMeals(loadedMeals);
+      setIsLoading(false);
     };
 
     fetchMeals();
-  }, [])
+  }, []);
+
+  if(isLoading) {
+    return (
+      <section className={classes.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    )
+  }
 
   const mealsList = meals.map(meal => <MealItem id={meal.id} key={meal.id} name={meal.name} description={meal.description} price={meal.price} />);
 
