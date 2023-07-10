5894d230e346e9bbe921447740b3dfb4ff00551a
sushant-dasuri <sushant.dasuri@more.global>
2023-07-10 20:53:10 +0530

    Handling http states & feedback with redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import store from './components/store/index';
+import store from './store/index';
 
 import './index.css';
 import App from './App';
