a7a0d6141eef71980d8dbd90875c003430a55e3c
sushant-dasuri <sushant.dasuri@more.global>
2023-07-12 16:56:07 +0530

    Layouts and nested routes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {
 } from 'react-router-dom';
 import HomePage from './pages/Home';
 import ProductsPage from './pages/Products';
+import RootLayout from './pages/Root';
 
 // const routeDefinitions = createRoutesFromElements(
 //   <Route>
@@ -15,8 +16,15 @@ import ProductsPage from './pages/Products';
 // )
 
 const router = createBrowserRouter([
-  {path: '/', element: <HomePage />},
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+       {path: '/', element: <HomePage />},
   {path: '/products', element: <ProductsPage />}
+    ]
+},
+ 
 ])
 
 // const router = createBrowserRouter(routeDefinitions);
