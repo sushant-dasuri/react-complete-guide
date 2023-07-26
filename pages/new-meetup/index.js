2806c5b10785a0f3111155d2f5c48d9d340e3330
sushant-dasuri <sushant.dasuri@more.global>
2023-07-26 19:41:42 +0530

    Adding a new meetup form

---
+++ pages/new-meetup/index.js
@@ -0,0 +1,13 @@
+import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+function NewMeetup() {
+
+    function addMeetupHandler(enteredMeetupData) {
+        console.log(enteredMeetupData);
+    }
+
+    return (
+        <NewMeetupForm onAddMeetup = {addMeetupHandler} />
+    )
+}
+
+export default NewMeetup;
