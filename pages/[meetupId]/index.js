d4f9ee90c62bf2f28d32194b613472e182b91b8b
sushant-dasuri <sushant.dasuri@more.global>
2023-07-27 23:58:18 +0530

    Working with params for SSG Data fetching

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -11,4 +11,20 @@ function MeetupDetails() {
     )
 }
 
+export async function getStaticProps(context) {
+
+   const meetupId =  context.params.meetupId;
+    return {
+        props: {
+            meetupData: {
+                image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg", 
+                id: meetupId,
+                title:"A First Meetup",
+                address:"Some address 5, 12345 Some City",
+                description:"This is a first meetup!",
+            }
+        }
+    }
+}
+
 export default MeetupDetails;
