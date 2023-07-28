93340538650e34e966387b7d662647b835edb2d9
sushant-dasuri <sushant.dasuri@more.global>
2023-07-28 20:29:31 +0530

    Working with mongodb

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,9 +1,20 @@
+import { MongoClient } from "mongodb";
 
-function handler(req, res) {
+async function handler(req, res) {
     if(req.method === 'POST') {
         const data = req.body;
 
-        const {title, image, address, description} = data
+        const client = await MongoClient.connect('mongodb+srv://sushant1184:ArOy330qbGLQueMn@cluster0.avdfjlp.mongodb.net/meetups?retryWrites=true&w=majority');
+        const db = cleint.db();
+
+        const meetupsCollection = db.collection('meetups');
+        const result = await meetupsCollection.insertOne(data);
+
+        console.log(result);
+
+        client.close();
+
+        res.status(201).json({message: 'Meetup Inserted'});
     }
 }
 
