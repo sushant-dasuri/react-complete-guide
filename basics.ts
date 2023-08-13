c59034887323b9815633878e25ee40919e460e21
sushant-dasuri <sushant.dasuri@more.global>
2023-08-13 23:54:08 +0530

    Exploring the base types

---
+++ basics.ts
@@ -0,0 +1,18 @@
+//Primitives: number, string, boolean
+//More Complex types: arrays, objects
+//Function types, parameters
+
+
+//Primitives
+
+let age: number;
+
+age = 12;
+
+let userName: string;
+
+userName = 'Max';
+
+let isInstructor: boolean;
+
+isInstructor = true;
