7125ccf1bb0372f8263279d1674be8b7272448d7
sushant-dasuri <sushant.dasuri@more.global>
2023-09-12 20:15:40 +0530

    Working with function props

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import Todos from './components/Todos';
 import Todo from './models/Todo';
+import NewTodo from './components/NewTodo';
 
 function App() {
   const todos = [
     new Todo('Learn React'),
     new Todo('Learn Typescript')
   ]
+
+  const addTodoHandler = (todoText: string) => {
+
+  }
   return (
     <div>
+      <NewTodo onAddTodo={addTodoHandler} />
       <Todos items={todos} />
     </div>
   );
--- react-ts/src/components/NewTodo.tsx
+++ react-ts/src/components/NewTodo.tsx
@@ -1,6 +1,6 @@
 import React, {useRef} from "react";
 
-const NewTodo = () => {
+const NewTodo: React.FC<{onAddTodo: (text: string) => void}> = (props) => {
 
     const todoTextInputRef = useRef<HTMLInputElement>(null)
 
@@ -12,6 +12,8 @@ const NewTodo = () => {
         if(enteredText.trim().length === 0) {
             return;
         }
+
+        props.onAddTodo(enteredText);
     }
 
     return (
