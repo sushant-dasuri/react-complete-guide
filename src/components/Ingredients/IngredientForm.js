92cbb455f4858a1052382c75a08fd8d79edb07e4
sushant-dasuri <sushant.dasuri@more.global>
2023-09-27 15:39:30 +0530

    More on useState and state updating

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -18,11 +18,11 @@ const inputState =  useState({title: '', amount: ''});
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" value={inputState[0].title} onChange={event => inputState[1]({title: event.target.value})} />
+            <input type="text" id="title" value={inputState[0].title} onChange={event => { const newTitle = event.target.value; inputState[1]((prevState) => ({title: newTitle, amount: prevState.amount}))}} />
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" value={inputState[0].amount} onChange={event => inputState[1]({amount: event.target.value})} />
+            <input type="number" id="amount" value={inputState[0].amount} onChange={event => { const newAmount = event.target.value; inputState[1]((prevState) => ({title: prevState.title, amount: newAmount}))}} />
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
